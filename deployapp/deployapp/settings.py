c6fd49fb3eb2e946d6a4f9425563bef9b8fac6ee
Jan <info@movie-match.com>
2018-04-14 20:09:44 +0200

    config heroku

--- deployapp/deployapp/settings.py
+++ deployapp/deployapp/settings.py
@@ -117,4 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+"""
+Addes for Heroku:
+"""
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
+
--- deployapp/deployapp/wsgi.py
+++ deployapp/deployapp/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deployapp.settings")
 
 application = get_wsgi_application()
+"""
+Added for Heroku:
+"""
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
