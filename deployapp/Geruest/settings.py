ad94a2b6721d39838df1b01e1aa789db3da81154
Jan <info@movie-match.com>
2018-04-16 00:36:27 +0200

    added django-heroku change to bottom of settings.py

--- deployapp/Geruest/settings.py
+++ deployapp/Geruest/settings.py
@@ -141,3 +141,5 @@ STATICFILES_DIRS = [
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
