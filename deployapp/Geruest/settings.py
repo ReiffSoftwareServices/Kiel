11334986fc9035086668760e86ff160bc0cf369f
Jan <info@movie-match.com>
2018-04-16 00:25:33 +0200

    db change

--- deployapp/Geruest/settings.py
+++ deployapp/Geruest/settings.py
@@ -82,13 +82,14 @@ DATABASES = {
 }
 """
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'mydatabase',
-    'USER': 'mydatabaseuser',
-    'PASSWORD': 'mypassword',
-    'HOST': '127.0.0.1',
-    'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
